5bc26a882583fc1cd3c20368a3d98df68122513a
Megalan <megalan4444@gmail.com>
2026-02-02 22:05:44 +0530

    Add testbench for OR gate module

---
+++ gates/or gate/or gate tb.v
@@ -0,0 +1,20 @@
+module or_gate_tb ();
+  reg a,b;
+  wire y;
+  or_gate dut(.a(a),.b(b),.y(y));
+  initial begin
+    a=0;b=0;
+    #2 a=0;b=1;
+    #2 a=1;b=0;
+    #2 a=1;b=1;
+    #2 $finish;
+  end
+  initial begin
+    $monitor(" time=%0t  ||A=%b B=%b  ||Y=%b",$time,a,b,y);
+  end
+  initial begin
+    $dumpfile("or_gate.vcd");
+    $dumpvars(1,or_gate_tb);
+    #10 $finish;
+  end
+endmodule
