49a3e9aa4d11b4313f7344261df577056de89a71
Megalan <megalan4444@gmail.com>
2026-02-02 22:14:10 +0530

    Remove waveform dumping from xor gate testbench
    Removed waveform dumping from the testbench.

--- gates/xor gate/xor gate tb.v
+++ gates/xor gate/xor gate tb.v
@@ -12,9 +12,4 @@ module xor_gate_tb ();
   initial begin
     $monitor(" time=%0t  ||A=%b B=%b  ||Y=%b",$time,a,b,y);
   end
-  initial begin
-    $dumpfile("xor_gate.vcd");
-    $dumpvars(1,xor_gate_tb);
-    #10 $finish;
-  end
 endmodule
