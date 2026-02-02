dbab19d8d11e998732199cce10f6fdd87f1c31ea
Megalan <megalan4444@gmail.com>
2026-02-02 22:00:09 +0530

    Add testbench for AND gate module

---
+++ gates/and gate/and gate tb.v
@@ -0,0 +1,21 @@
+module and_gate_tb ();
+  reg a,b;
+  wire y;
+  and_gate dut(.a(a),.b(b),.y(y));
+  initial begin
+    a=0;b=0;
+    #2 a=0;b=1;
+    #2 a=1;b=0;
+    #2 a=1;b=1;
+    #2 $finish;
+  end
+  initial begin
+    $monitor(" time=%0t  ||A=%b B=%b  ||Y=%b",$time,a,b,y);
+  end
+  initial begin
+    $dumpfile("and_gate.vcd");
+    $dumpvars(1,and_gate_tb);
+    #10
+    $finish;
+  end
+endmodule
