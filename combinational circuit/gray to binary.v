5a3df6dd26288eb992f6f542cab35c8143d96e51
Megalan <megalan4444@gmail.com>
2026-02-05 19:57:29 +0530

    Add Gray to Binary conversion module

---
+++ combinational circuit/gray to binary.v
@@ -0,0 +1,9 @@
+module gray_to_binary(input[3:0]g,
+                      output[3:0]b);
+  assign b[3]=g[3];
+  assign b[2]=g[3]^g[2];
+  assign b[1]=g[3]^g[2]^g[1];
+  assign b[0]=g[3]^g[2]^g[1]^g[0];
+endmodule
+    
+      
