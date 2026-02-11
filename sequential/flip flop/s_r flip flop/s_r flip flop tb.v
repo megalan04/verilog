80e3bd7111e34fbc7147f2b525179b7992df00c0
Megalan <megalan4444@gmail.com>
2026-02-11 21:37:51 +0530

    Update s_r flip flop tb.v

--- sequential/flip flop/s_r flip flop/s_r flip flop tb.v
+++ sequential/flip flop/s_r flip flop/s_r flip flop tb.v
@@ -1,24 +1,22 @@
 module tb;
-  reg j,k,clk,rst;
+  reg s,r,clk,rst;
   wire q;
-  jk_flip_flop DUT(j,k,clk,rst,q);
+  sr_flip_flop DUT(s,r,clk,rst,q);
   initial 
      clk=0;
   always #5 clk=~clk;
   initial begin
    
-    rst=1;j=1;k=1;
-    #20 rst=0;j=0;k=0;
-    #20 j=0;k=1;
-    #20 j=1;k=0;
-    #20 j=1;k=1;
-     #20 j=1;k=0;
+    rst=1;s=1;r=1;
+    #20 rst=0;s=0;r=0;
+    #20 s=0;r=1;
+    #20 s=1;r=0;
+    #20 s=1;r=1;
+     #20 s=1;r=0;
      
     #20; $finish;
   end
   initial begin
-    $monitor("time=%0t INPUT VALUES:clk=%b rst=%b j=%b k=%b,OUTPUT VALUES:q=%b",$time,clk,rst,j,k,q);
-    $dumpfile("jk_flip_flop.vcd");
-    $dumpvars;
+    $monitor("time=%0t INPUT VALUES:clk=%b rst=%b s=%b r=%b,OUTPUT VALUES:q=%b",$time,clk,rst,s,r,q);
   end
 endmodule
