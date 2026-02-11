00af3dc7ab19fb296e338bafdd1c276cdd6ea3f2
Megalan <megalan4444@gmail.com>
2026-02-11 21:40:31 +0530

    Create j_k flip flop syn.v

---
+++ sequential/flip flop/j_k flip flop/j_k flip flop syn.v
@@ -0,0 +1,15 @@
+module jk_flip_flop(input j,k,clk,rst,
+                    output reg q);
+  always@(posedge clk )begin
+    if(rst)
+      q<= 1'b0;
+    else begin
+      case ({j,k})
+       0:q<=q;
+       1:q<=0;
+       2:q<=1;
+       default:q<=~q;
+     endcase
+    end
+  end
+endmodule
