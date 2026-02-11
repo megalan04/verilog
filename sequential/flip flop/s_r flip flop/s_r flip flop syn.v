1ff134ec25fb53a87c2fadfea7df3cbf13814252
Megalan <megalan4444@gmail.com>
2026-02-11 21:33:32 +0530

    Create s_r flip flop syn.v

---
+++ sequential/flip flop/s_r flip flop/s_r flip flop syn.v
@@ -0,0 +1,15 @@
+module sr_flip_flop(input s,r,clk,rst,
+                    output reg q);
+  always@(posedge clk )begin
+    if(rst)
+      q<= 1'b0;
+    else begin
+     case ({s,r})
+       0:q<=q;
+       1:q<=0;
+       2:q<=1;
+       default:q<=1'bx;
+     endcase
+    end
+  end
+endmodule
