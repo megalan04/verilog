045e556937d104d1afcd6ea3fc530789e75812a2
Megalan <megalan4444@gmail.com>
2026-02-16 10:44:54 +0530

    Delete sequential/counter/ring counter/johnson counter.v

--- sequential/counter/ring counter/johnson counter.v
+++
@@ -1,9 +0,0 @@
-module johnson_counter(input clk,rst,
-                    output reg [3:0]q);
-  always@(posedge clk)begin
-    if (rst)
-      q<= 4'b0001;
-    else
-      q<={q[2:0],~q[3]};
-  end
-endmodule
