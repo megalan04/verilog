6624597cc92f1d61f559e3b976c507c9485a342c
Megalan <megalan4444@gmail.com>
2026-02-16 12:23:19 +0530

    Create johnson asyn_reset_counter.v

---
+++ sequential/counter/johnson counter/johnson asyn_reset_counter.v
@@ -0,0 +1,9 @@
+module johnson_counter(input clk,rst,
+                    output reg [3:0]q);
+  always@(posedge clk or posedege rst)begin
+    if (rst)
+      q<= 4'b0001;
+    else
+      q<={q[2:0],~q[3]};
+  end
+endmodule
