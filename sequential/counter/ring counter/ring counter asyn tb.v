b6080a9bd66e785d23cf44d2f43ed45682b1c182
Megalan <megalan4444@gmail.com>
2026-02-16 10:28:23 +0530

    Create ring counter asyn tb.v

---
+++ sequential/counter/ring counter/ring counter asyn tb.v
@@ -0,0 +1,20 @@
+module tb;
+  reg clk;
+  reg rst;
+  wire[3:0]q;
+  ring_counter DUT(.*);
+  always #2 clk=~clk;
+  initial begin
+    #5 rst=0;
+    #5 clk=0;
+    #5 rst=1;
+    #5 rst=0;
+    #30 $finish;
+  end
+    initial begin
+      $monitor("time=%0t, INPUT VALUES:clk=%b, rst=%b, OUTPUT VALUES:q=%b",$time,clk,rst,q);
+      $dumpfile("ring_counter.vcd");
+      $dumpvars;
+    end
+endmodule
+    
