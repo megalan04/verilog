a7aabc46084cc80809876479c1fd5a2e068de26c
Megalan <megalan4444@gmail.com>
2026-02-18 18:04:48 +0530

    Update octal to binary encoder tb.v

--- combinational circuit/octal to binary encoder/octal to binary encoder tb.v
+++ combinational circuit/octal to binary encoder/octal to binary encoder tb.v
@@ -1,17 +1,19 @@
-module octal_to_binary_encoder(input[7:0]d,
-                               output reg[2:0]y);
-  always@(*)begin
-    case(d)
-       8'b0000_0001:y=3'd0;
-       8'b0000_0010:y=3'd1;
-       8'b0000_0100:y=3'd2;
-       8'b0000_1000:y=3'd3;
-       8'b0001_0000:y=3'd4;
-       8'b0010_0000:y=3'd5;
-       8'b0100_0000:y=3'd6;
-       8'b1000_0000:y=3'd7;
-      default:$display("INVALID");
-    endcase 
-  end
+module test;
+  reg[7:0]d;
+  wire[2:0]y;
+  octal_to_binary_encoder DUT(.*);
+  initial begin
+  #2 d=0; 
+  #2 d=8'b0000_0001;
+  #2 d=8'b0000_0010;
+  #2 d=8'b0000_0100; 
+  #2 d=8'b0000_1000;
+  #2 d=8'b0001_0000;
+  #2 d=8'b0010_0000;
+  #2 d=8'b0100_0000;
+  #2 d=8'b1000_0000;
+  #60 $finish;
+  end  
+  always@(*)
+    $display("time=%0t INPUT VALUES: d=%b OUTPUT VALUES: y=%b",$time,d,y);
 endmodule
-  
