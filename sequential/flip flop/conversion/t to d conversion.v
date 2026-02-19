5005c20ef4ac86c90522261d443e6c35b53b0401
Megalan <megalan4444@gmail.com>
2026-02-19 12:55:01 +0530

    Update t to d conversion.v

--- sequential/flip flop/conversion/t to d conversion.v
+++ sequential/flip flop/conversion/t to d conversion.v
@@ -1,13 +1,22 @@
+module t_ff (input t,clk,rst,
+             output reg q);
+  always @(posedge clk or posedge rst ) begin
+    if (rst)
+        q <= 1'b0;
+    else begin
+      if(t)
+        q<=~q;
+      else
+        q<=q;
+    end
+  end
+endmodule
+
 module t_2_d (input clk,
     input rst,
     input d,
     output reg q);
 wire t;
 assign t=d^q;
-always @(posedge clk or posedge rst) begin
-    if (rst)
-        q<=1'b0;
-    else if(t)
-        q<=~q;
-end
+  t_ff d_ff(.t(t),.clk(clk),.rst(rst),.q(q));
 endmodule
