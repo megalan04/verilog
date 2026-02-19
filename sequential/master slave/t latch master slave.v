8e4f71d480ea2682e1798e914fef42fc2dffb79e
Megalan <megalan4444@gmail.com>
2026-02-19 17:11:12 +0530

    Update t latch master slave.v

--- sequential/master slave/t latch master slave.v
+++ sequential/master slave/t latch master slave.v
@@ -10,8 +10,20 @@ module t_latch(
     end
   end
 endmodule
+module ts_latch(
+  input t,en,
+  output reg q=0);
+  always@(*)begin
+    if(!en)begin
+      if(t)
+        q=~q;
+      else
+        q=q;
+    end
+  end
+endmodule
 module t_master_slave(input t,clk,rst,
                        output reg qm,qs);
   t_latch master(.t(t),.en(clk),.q(qm));
-  t_latch slave(.t(qm),.en(~clk),.q(qs));
+  ts_latch slave(.t(qm),.en(clk),.q(qs));
 endmodule
