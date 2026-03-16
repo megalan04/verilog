35fdafc7ee7603606ec07d56bb67b407d977a8d4
Megalan <megalan4444@gmail.com>
2026-03-16 17:38:02 +0530

    Create syn_fifo_with.v

---
+++ sequential/FIFO/syn_fifo_with.v
@@ -0,0 +1,34 @@
+module syn_fifo(input clk,
+               rst,
+               w_en,r_en,
+               input[7:0]data_in,
+               output reg [7:0] data_out
+               );
+  reg[7:0]mem[0:7];
+  reg [2:0] wptr,rptr;
+   wire full,empty;
+  always@(posedge clk)begin
+    if(rst)begin
+      wptr<=3'b0;
+      data_out<=3'b0;
+    end
+    else if(w_en && !full)begin
+       mem[wptr]<=data_in;
+           wptr<=wptr+1;
+    end
+  end
+   always@(posedge clk)begin
+    if(rst)begin
+      rptr<=3'b0;
+      data_out<=8'b0;
+    end
+     else if(r_en && !empty)begin
+       data_out<=mem[rptr];
+           rptr<=rptr+1;
+    end
+  end
+  assign full=(rptr==wptr+1);
+  assign empty=(wptr==rptr);
+endmodule
+  
+
