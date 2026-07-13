15a0cdf273fe7d0a491a8e88cbd26905f2b56bf1
Megalan <megalan4444@gmail.com>
2026-07-13 18:25:34 +0530

    Update memory.v

--- sequential/FIFO/ASYN FIFO/memory.v
+++ sequential/FIFO/ASYN FIFO/memory.v
@@ -1,18 +1,21 @@
-module memory #(parameter w=8 ,d=3) (
-  input w_clk,r_clk,w_en,r_en,full,empty,
-  input [d:0]wptr_b,rptr_b,
-  input [w-1:0]d_in,
-  output [w-1:0]d_out
-);
-  
-reg [w-1:0]mem[w-1:0];
-  
-always @(posedge w_clk)begin
-  if(w_en && !full)begin
-    mem[wptr_b[d-1:0]] <= d_in;
-  end
+module fifo_mem #(parameter d_width=8,depth=8,ptr_width=4)
+(input wclk,rclk,we,re,full,empty,
+input [ptr_width-1:0]b_wptr,b_rptr,
+input [d_width-1:0]w_data,
+output reg [d_width-1:0]r_dout);
+reg [d_width-1:0]fifo_mem[depth-1:0];
+always @(posedge wclk)
+begin
+	if( we && !full)
+	begin
+		fifo_mem[b_wptr] <= w_data;
+	end
 end
-  
-assign d_out = mem[rptr_b[d-1:0]];
-  
+always @(posedge rclk)
+begin
+	if(re && !empty)
+	begin
+		r_dout <= fifo_mem[b_rptr];
+	end
+end
 endmodule
